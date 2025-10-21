ccb8a3100f0db5c37f595f968d964f711ac6661a
Plucci78 <pako.lucci@gmail.com>
2025-10-21 21:59:43 +0200

    fix: Configura Strapi per proxy HTTPS su Render
    - Aggiunto url pubblico per generare link corretti
    - Abilitato proxy mode per gestire cookie sicuri dietro reverse proxy
    - Fix errore 'Cannot send secure cookie over unencrypted connection'

---
+++ cms/config/server.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  url: env('PUBLIC_URL', 'https://omnilypro.onrender.com'),
+  proxy: env.bool('IS_PROXIED', true),
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
+  },
+});
